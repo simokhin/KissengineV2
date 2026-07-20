3f521b699bd0987970691b27106f32a32a5fd8d0
Nikita Simokhin <just.simokhin@gmail.com>
2026-07-21 01:53:26 +0300

    Add pst for King in the endgame

--- engine/eval.go
+++ engine/eval.go
@@ -1,5 +1,7 @@
 package engine
 
+const totalPhase = 24
+
 var pieceValues = [7]int{
 	Pawn:   100,
 	Knight: 320,
@@ -20,22 +22,57 @@ var pst = [7][64]int{
 	King:   kingPST,
 }
 
+var phaseWeights = [7]int{
+	Knight: 1,
+	Bishop: 1,
+	Rook:   2,
+	Queen:  4,
+}
+
 func Evaluate(pos Position) int {
 	var eval int
 
+	phase := gamePhase(pos)
+
 	for pt := Pawn; pt <= King; pt++ {
+
 		whitePieces := pos.Pieces[pt] & pos.Colors[White]
 		for whitePieces != 0 {
 			sq := whitePieces.PopLSB()
-			eval += pieceValues[pt] + pst[pt][sq^56]
+
+			// Change PST for King in the endgame
+			pstValue := pst[pt][sq^56]
+			if pt == King {
+				pstValue = (pst[King][sq^56]*phase + kingEndgamePST[sq^56]*(totalPhase-phase)) / totalPhase
+			}
+
+			eval += pieceValues[pt] + pstValue
 		}
 
 		blackPieces := pos.Pieces[pt] & pos.Colors[Black]
 		for blackPieces != 0 {
 			sq := blackPieces.PopLSB()
-			eval -= pieceValues[pt] + pst[pt][sq]
+
+			// Change PST for King in the endgame
+			pstValue := pst[pt][sq]
+			if pt == King {
+				pstValue = (pst[King][sq]*phase + kingEndgamePST[sq]*(totalPhase-phase)) / totalPhase
+			}
+
+			eval -= pieceValues[pt] + pstValue
 		}
 	}
 
 	return eval
+}
+
+func gamePhase(pos Position) int {
+	var phase int
+
+	for pt := Pawn; pt <= King; pt++ {
+		pieces := pos.Pieces[pt]
+		phase += pieces.PopCount() * phaseWeights[pt]
+	}
+
+	return min(phase, totalPhase)
 }
