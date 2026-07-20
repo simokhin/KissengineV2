b478137819318599a3c8fcc1ec535881984f9573
Nikita Simokhin <just.simokhin@gmail.com>
2026-07-20 04:40:55 +0300

    Add PST

--- engine/eval.go
+++ engine/eval.go
@@ -9,13 +9,32 @@ var pieceValues = [7]int{
 	King:   0,
 }
 
+// pst holds piece-square tables indexed by piece type, giving each square
+// a positional bonus/penalty from White's perspective (mirror via sq^56 for Black)
+var pst = [7][64]int{
+	Pawn:   pawnPST,
+	Knight: knightPST,
+	Bishop: bishopPST,
+	Rook:   rookPST,
+	Queen:  queenPST,
+	King:   kingPST,
+}
+
 func Evaluate(pos Position) int {
 	var eval int
 
-	for pt := Pawn; pt <= Queen; pt++ {
-		whiteCount := (pos.Pieces[pt] & pos.Colors[White]).PopCount()
-		blackCount := (pos.Pieces[pt] & pos.Colors[Black]).PopCount()
-		eval += (whiteCount - blackCount) * pieceValues[pt]
+	for pt := Pawn; pt <= King; pt++ {
+		whitePieces := pos.Pieces[pt] & pos.Colors[White]
+		for whitePieces != 0 {
+			sq := whitePieces.PopLSB()
+			eval += pieceValues[pt] + pst[pt][sq^56]
+		}
+
+		blackPieces := pos.Pieces[pt] & pos.Colors[Black]
+		for blackPieces != 0 {
+			sq := blackPieces.PopLSB()
+			eval -= pieceValues[pt] + pst[pt][sq]
+		}
 	}
 
 	return eval
