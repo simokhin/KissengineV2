bbbe76cc58526e9564002e41adea9c88a0c0ac35
Nikita Simokhin <just.simokhin@gmail.com>
2026-07-20 16:04:22 +0300

    Add function to create zobrist hash of the position

---
+++ engine/zobrist.go
@@ -0,0 +1,70 @@
+package engine
+
+import "math/rand"
+
+// zobristPieces holds random keys for each (color, piece type, square)
+// combination, used to build a Zobrist hash of a position.
+var zobristPieces [2][6][64]uint64
+
+// zobristSideToMove is XORed into the hash when it's Black's turn to move.
+var zobristSideToMove uint64
+
+// zobristCastling holds random keys for each of the four castling rights.
+var zobristCastling [4]uint64
+
+// zobristEnPassantFile holds random keys for each file an en passant
+// capture might be available on.
+var zobristEnPassantFile [8]uint64
+
+// Hash computes the Zobrist hash of the position from scratch, combining
+// piece placement, side to move, castling rights, and en passant file.
+func (p *Position) Hash() uint64 {
+	var hash uint64
+
+	for color := range 2 {
+		for pt := range 6 {
+			pieces := p.Pieces[pt] & p.Colors[color]
+			for pieces != 0 {
+				sq := pieces.PopLSB()
+				hash ^= zobristPieces[color][pt][sq]
+			}
+		}
+	}
+
+	if p.SideToMove == Black {
+		hash ^= zobristSideToMove
+	}
+
+	for castle := range 4 {
+		if p.Castling&(1<<castle) != 0 {
+			hash ^= zobristCastling[castle]
+		}
+	}
+
+	if p.EnPassant != NoSquare {
+		hash ^= zobristEnPassantFile[p.EnPassant.File()]
+	}
+
+	return hash
+}
+
+// init fills the Zobrist key tables with random values.
+func init() {
+	for color := range 2 {
+		for pt := range 6 {
+			for sq := range 64 {
+				zobristPieces[color][pt][sq] = rand.Uint64()
+			}
+		}
+	}
+
+	zobristSideToMove = rand.Uint64()
+
+	for castle := range 4 {
+		zobristCastling[castle] = rand.Uint64()
+	}
+
+	for file := range 8 {
+		zobristEnPassantFile[file] = rand.Uint64()
+	}
+}
